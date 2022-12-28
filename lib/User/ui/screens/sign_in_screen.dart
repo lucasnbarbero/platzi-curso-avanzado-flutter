aa530ff8d5e2b75189b77311491bcfbf047124f8
Lucas Barbero <lucas95_273@hotmail.com>
2022-12-28 10:35:37 -0300

    Implementacion del boton singingWithGoogle

--- lib/User/ui/screens/sign_in_screen.dart
+++ lib/User/ui/screens/sign_in_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../../widgets/gradient_back.dart';
+import '../../../widgets/button_green.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -20,15 +21,19 @@ class _SignInScreenState extends State<SignInScreen> {
         children: <Widget>[
           GradientBack('', null),
           Column(
-            children: const <Widget>[
-              Text(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Text(
                 'Welcome \n This is your Travel App',
                 style: TextStyle(
                     fontFamily: "Lato",
                     fontSize: 37.0,
                     color: Colors.white,
                     fontWeight: FontWeight.bold),
-              )
+              ),
+              ButtonGreen(50.0, 300.0,
+                  text: 'Login with Gmail', onPressed: () {})
             ],
           ),
         ],
